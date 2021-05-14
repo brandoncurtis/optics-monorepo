10ffeb51f635dbc6f33f6e2b8809a4b4463ee518
Erin Hales <mcbridee093@gmail.com>
2021-05-14 09:35:17 -0600

    remove: getUpgradeBeacon func

--- solidity/optics-core/test/cross-chain/GovernanceRouter.test.js
+++ solidity/optics-core/test/cross-chain/GovernanceRouter.test.js
@@ -8,7 +8,6 @@ const {
   getHome,
   getReplica,
   getGovernanceRouter,
-  getUpgradeBeacon,
   getUpgradeBeaconController,
 } = require('./deployCrossChainTest');
 
@@ -364,8 +363,7 @@ describe('GovernanceRouter', async () => {
       'MysteryMathV1',
     );
     mysteryMathProxy = contracts.proxyWithImplementation;
-    upgradeBeacon = getUpgradeBeacon(chainDetails, governorDomain);
-    // upgradeBeacon = contracts.upgradeBeacon;
+    upgradeBeacon = contracts.upgradeBeacon;
     upgradeBeaconController = getUpgradeBeaconController(
       chainDetails,
       governorDomain,
